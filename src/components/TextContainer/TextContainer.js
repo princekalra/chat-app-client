03b3be72f1494d410fc6b12ae63e061ff3c411c0
princekalra <kalraprince75@gmail.com>
2021-05-29 08:47:48 -0700

    thierd coomit

--- src/components/TextContainer/TextContainer.js
+++ src/components/TextContainer/TextContainer.js
@@ -7,8 +7,8 @@ import './TextContainer.css';
 const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
-      <h1>Realtime Chat Application <span  aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span aria-label="emoji">😎</span></h2>
+      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">😎</span></h2>
       <h2>Devloped By Prince... </h2>
     </div>
     {
